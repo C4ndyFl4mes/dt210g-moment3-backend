5eb1098136b107158d41a0c09781e4e7213bc038
Isaac Svedin <swedinisaac@gmail.com>
2026-02-06 08:25:00 +0100

    Debugging the start of the application.

--- App/Program.cs
+++ App/Program.cs
@@ -11,13 +11,19 @@ using Microsoft.IdentityModel.Tokens;
 
 DotNetEnv.Env.Load();
 
+System.Console.WriteLine("Starting application... DEBUG");
+
 var builder = WebApplication.CreateBuilder(args);
+
+System.Console.WriteLine("Configuring services... DEBUG CREATEBUILDER");
 
 var env = builder.Environment;
 if (env.IsDevelopment())
 {
     builder.Configuration.AddEnvironmentVariables();
 }
+
+System.Console.WriteLine("Environment variables loaded... DEBUG ENV");
 
 var host = Environment.GetEnvironmentVariable("MYSQLHOST")
     ?? throw new Exception("MYSQLHOST not set");
@@ -36,7 +42,7 @@ var password = Environment.GetEnvironmentVariable("MYSQLPASSWORD")
 
 var connectionString = $"Server={host};Port={port};Database={database};User={user};Password={password};";
 
-System.Console.WriteLine(connectionString);
+System.Console.WriteLine($"Connection string constructed... DEBUG CONNECTIONSTRING: {connectionString}...");
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
